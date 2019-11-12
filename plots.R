39e1be5006b00d0a11e9a593ab2dab162decc4f5
michaelofrancis <51752343+michaelofrancis@users.noreply.github.com>
2019-11-12 13:00:04 -0500

    Update plots.R

--- plots.R
+++ plots.R
@@ -1,15 +1,15 @@
 #PBS -S /bin/bash
 #PBS -q ye_q
 #PBS -N plots
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=4
 #PBS -l walltime=1:00:00:00
-#PBS -l mem=8gb
+#PBS -l mem=20gb
 
 #PBS -M michaelfrancis@uga.edu
 #PBS -m abe
 
 #Load R
-ml R/3.6.1-foss-2018a-X11-20180131-GACRC
+#ml R/3.6.1-foss-2018a-X11-20180131-GACRC
 
 library(qqman)
 library(plyr)
@@ -34,46 +34,24 @@ possible_dirs<-sprintf("./%s_par%s", phenotypes, par)
 
 dirs_exist<-possible_dirs[possible_dirs %in% dirs]
 
-pdf(file= test.pdf)
+png(file= "test5.png")
+par(mfrow=c(2,2))
 
 for (i in seq_along(dirs_exist)){
-  if(sum(grepl("*full.assoc.logistic", list.files(path= dirs_exist[i])))==1){
-    filename<-paste(dirs_exist[i], "/", grep("*full.assoc.logistic", list.files(path= dirs_exist[i]), value = TRUE), sep="")
-      #For par 22
-      if(grepl("par22", filename)){
-      data<-read.table(filename, header=TRUE, stringsAsFactors=FALSE)
-      data<--data%>%filter(TEST == "ADDxweekly_oily_fish")
-      data<-data %>% select(CHR, SNP, BP, P)
-      data[,-2]<-sapply(data[,-2], as.numeric)
-      data<-drop_na(data)
-      manhattan(data, 
-          main = "Title goes here", 
-          ylim = c(0, 10), cex = 0.6, 
-          annotatePval = 0.00001, annotateTop = FALSE,
-          cex.axis = 0.9, col = c("red", "black"), 
-          suggestiveline = -log10(1e-05), genomewideline = -log10(5e-08), 
-)}
-      else if(grepl("par24", filename)){
-      data<--data%>%filter(TEST == "ADDxfish_oil_initial")
-      data<-data %>% select(CHR, SNP, BP, P)
-      data[,-2]<-sapply(data[,-2], as.numeric)
-      data<-drop_na(data)
-      manhattan(data, 
-          main = "Title goes here", 
-          ylim = c(0, 10), cex = 0.6, 
-          annotatePval = 0.00001, annotateTop = FALSE,
-          cex.axis = 0.9, col = c("red", "black"), 
-          suggestiveline = -log10(1e-05), genomewideline = -log10(5e-08), 
-)}
-      
-      
-      
-      
-      
-      
-            }}
+  if(sum(grepl("*fullc.assoc.logistic", list.files(path= dirs_exist[i])))==1){
+     filename<-paste(dirs_exist[i], "/", grep("*fullc.assoc.logistic", list.files(path= dirs_exist[i]), value = TRUE), sep="")
+	data<-read.table(filename, header=FALSE, stringsAsFactors=FALSE)
+	colnames(data)= c("CHR", "SNP", "BP", "P")
+	data[,-2]<-sapply(data[,-2], as.numeric)
+	data<-drop_na(data)
+	manhattan(data, 
+		main = paste(filename), 
+		ylim = c(0, 10), cex = 0.6, 
+		annotatePval = 0.00001, annotateTop = FALSE,
+		cex.axis = 0.9, col = c("red", "black"), 
+		suggestiveline = -log10(1e-05), genomewideline = -log10(5e-08),
+		chrlabs = c(1:22, "X", "XY") 
+)
+  
+          }}
 dev.off()
-        
-
-
-
