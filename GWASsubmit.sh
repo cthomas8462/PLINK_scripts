4cd8d1efa6013a0e8ce76bb09188623ede761835
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2019-12-15 17:32:00 -0500

    Update GWASsubmit.sh

--- GWASsubmit.sh
+++ GWASsubmit.sh
@@ -5,10 +5,8 @@
 #PBS -l walltime=30:00:00:00
 #PBS -l mem=5gb
 
-#PBS -M michaelfrancis@uga.edu
-#PBS -m ae
 
-cd /work/kylab/share/UKB/scripts_round7_11202019
+cd /work/kylab/share/UKB/scripts_round8_12132019
 
 phenotypes=("Depress_2wk" "breast_cancer" "prostate_cancer" \ 
 "LI_colorectal_cancer" "colon_sigmoid_cancer" "liver_cancer" \ 
@@ -19,7 +17,6 @@ phenotypes=("Depress_2wk" "breast_cancer" "prostate_cancer" \
 "cardiac.12" "cardiac.13" "cardiac.14" "cardiac.15" "cardiac.16" \
 "cardiac.17" "cardiac.18" "cardiac.19" "cardiac.20" "cardiac.21")
 
-chr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X XY)
 par=(22 24)
 
 
@@ -27,12 +24,9 @@ for j in ${phenotypes[@]}
 	do
 	for k in ${par[*]}
 		do
-		for i in ${chr[*]}
-			do
 
 
-qsub /work/kylab/share/UKB/scripts_round7_11202019/manyscripts/"$j"_par"$k"_chr"$i".sh
+qsub /work/kylab/share/UKB/scripts_round8_12132019/manyscripts/"$j"_par"$k".sh
 
-done
 done
 done
