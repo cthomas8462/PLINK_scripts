c7b2853bbe18fb76aac3ee2f4ee5178a5bdadfda
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-03 15:57:11 -0500

    Update sortB.sh

--- sortB.sh
+++ sortB.sh
@@ -1,12 +1,9 @@
 #PBS -S /bin/bash
 #PBS -q ye_q
 #PBS -N sort
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=2
 #PBS -l walltime=1:00:00:00
 #PBS -l mem=8gb
-
-#PBS -M michaelfrancis@uga.edu
-#PBS -m abe
 
 cd /scratch/mf91122/UKBimputation/GWAS_results_11222019
 
@@ -23,8 +20,7 @@ chr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X XY)
 
 par=(22 24)
 
-###SORT===============================================================
-
+#sort data
 echo Sorting data...
 for i in ${chr[*]}
         do
@@ -49,13 +45,6 @@ done
 cd /scratch/mf91122/UKBimputation/GWAS_results_11222019
 rmdir * 2>/dev/null
 
-###COMBINE====================================================
-
-echo Removing files of size zero (initial pass)...
-cd /scratch/mf91122/UKBimputation/GWAS_results_11222019
-find ./ -size  0 -print0 |xargs -0 rm --
-
-
 #check if files exist
 
 for j in ${phenotypes[@]} 
@@ -77,21 +66,7 @@ if [ -d "$j"_par"$k" ]
 		echo Combining "${filecount}" files in "$j"_par"$k"
 		cat * > "$j"_par"$k"_full.assoc.logistic
 	fi	
-
-done; done
-
-
-for j in ${phenotypes[@]} 
-	do
-		for k in ${par[*]}
-			do
-
-cd /scratch/mf91122/UKBimputation/GWAS_results_11222019
-#if directory exists then go into that directory
-if [ -d "$j"_par"$k" ]
-	then
-	cd "$j"_par"$k"
-
+	
 	if [ "$filecount" == 25 -a "$k" == 22 ]
 	then
 			echo Processing "${filecount}" files in "$j"_par"$k"...
@@ -111,8 +86,9 @@ if [ -d "$j"_par"$k" ]
 	fi
 
 fi
-done; done
+done
+done
 
-echo Removing files of size zero (final pass)...
+echo Removing files of size zero...
 cd /scratch/mf91122/UKBimputation/GWAS_results_11222019
 find ./ -size  0 -print0 |xargs -0 rm --
