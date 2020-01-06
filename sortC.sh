cba5f8e17a4afbad4827a68bcbfc8dfff85e441d
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-05 20:33:18 -0500

    Update sortC.sh

--- sortC.sh
+++ sortC.sh
@@ -5,7 +5,7 @@
 #PBS -l walltime=1:00:00:00
 #PBS -l mem=8gb
 
-cd /scratch/mf91122/UKBimputation/8.GWAS_results_12142019
+cd /scratch/mf91122/UKBimputation/11.GWAS-results_01-04-2020_FO_24_std
 
 phenotypes=("Depress_2wk" "breast_cancer" "prostate_cancer" \ 
 "LI_colorectal_cancer" "colon_sigmoid_cancer" "liver_cancer" \ 
@@ -14,11 +14,12 @@ phenotypes=("Depress_2wk" "breast_cancer" "prostate_cancer" \
 "cardiac.2" "cardiac.3" "cardiac.4" "cardiac.5" "cardiac.6" \
 "cardiac.7" "cardiac.8" "cardiac.9" "cardiac.10" "cardiac.11" \
 "cardiac.12" "cardiac.13" "cardiac.14" "cardiac.15" "cardiac.16" \
-"cardiac.17" "cardiac.18" "cardiac.19" "cardiac.20" "cardiac.21")
+"cardiac.17" "cardiac.18" "cardiac.19" "cardiac.20" "cardiac.21" \
+"LDL" "TAGs" "Tot_Chol")
 
 chr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X XY)
 
-par=(22 24)
+par=(35)
 
 ###SORT===============================================================
 
@@ -44,14 +45,13 @@ done
 done
 done
 
-cd /scratch/mf91122/UKBimputation/8.GWAS_results_12142019
+cd /scratch/mf91122/UKBimputation/11.GWAS-results_01-04-2020_FO_24_std
 rmdir * 2>/dev/null
-
 
 ###COMBINE====================================================
 
-echo Removing files of size zero (initial pass)...
-cd /scratch/mf91122/UKBimputation/8.GWAS_results_12142019
+echo Removing files of size zero--initial pass...
+cd /scratch/mf91122/UKBimputation/11.GWAS-results_01-04-2020_FO_24_std
 find ./ -size  0 -print0 |xargs -0 rm --
 
 
@@ -62,49 +62,29 @@ for j in ${phenotypes[@]}
 		for k in ${par[*]}
 			do
 
-cd /scratch/mf91122/UKBimputation/8.GWAS_results_12142019
+cd /scratch/mf91122/UKBimputation/11.GWAS-results_01-04-2020_FO_24_std
 #if directory exists then go into that directory
 if [ -d "$j"_par"$k" ]
 	then
 	cd "$j"_par"$k"
 
-	filecount=$(ls|wc -l)
+	filecount=$(ls | wc -l)
 	echo filecount for "$j"_par"$k" is "$filecount"
 
 	if [ "$filecount" == 24 ]
 	then
 		echo Combining "${filecount}" files in "$j"_par"$k"
-		cat * > "$j"_par"$k".full
-	fi	
+		cat * > "$j"_par"$k"_full
+	fi
 
-done; done
-
-
-for j in ${phenotypes[@]} 
-	do
-		for k in ${par[*]}
-			do
-
-cd /scratch/mf91122/UKBimputation/8.GWAS_results_12142019
-#if directory exists then go into that directory
-if [ -d "$j"_par"$k" ]
-	then
-	cd "$j"_par"$k"
-
-	if [ "$filecount" == 25 -a "$k" == 22 ]
+	if [ "$filecount" == 25 -a "$k" == 35 ]
 	then
 			echo Processing "${filecount}" files in "$j"_par"$k"...
-			awk -F " " '{if ($5=="ADDxweekly_oily_fish")print $0}' "$j"_par22.full > \
-				"$j"_par22.fullb
-        		awk '{print $1,$2,$3,$9}' "$j"_par22.fullb > \
-		        	"$j"_par22.fullc
-	elif [ "$filecount" == 25 -a "$k" == 24 ]
-	then
-			echo Processing "${filecount}" files in "$j"_par"$k"...
-			awk -F " " '{if ($5=="ADDxfish_oil_initial")print $0}' "$j"_par24.full > \
-                        	"$j"_par24.fullb
-                	awk '{print $1,$2,$3,$9}' "$j"_par24.fullb > \
-                        	"$j"_par24.fullc
+			awk -F " " '{if ($7=="ADDxFish_Oil_Supp")print $0}' "$j"_par"$k"_full > \
+				"$j"_par"$k".fullb
+        		awk '{print $1,$3,$2,$12}' "$j"_par"$k".fullb > \
+		        	"$j"_par"$k".fullc
+		echo "$j"_par"$k" completed.
 	else 
 		echo There are "${filecount}" files in "$j"_par"$k"
 	fi
@@ -112,6 +92,6 @@ if [ -d "$j"_par"$k" ]
 fi
 done; done
 
-echo Removing files of size zero (final pass)...
-cd /scratch/mf91122/UKBimputation/8.GWAS_results_12142019
+echo Removing files of size zero--final pass...
+cd /scratch/mf91122/UKBimputation/11.GWAS-results_01-04-2020_FO_24_std
 find ./ -size  0 -print0 |xargs -0 rm --
