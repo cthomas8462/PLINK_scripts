2078273645ef118b184f54ba69695827e3c675cc
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-06 11:19:07 -0500

    Create ARIC_browse.R

---
+++ ARIC_browse.R
@@ -0,0 +1,10 @@
+#ml R/3.6.1-foss-2018a-X11-20180131-GACRC
+#look at files in /scratch/mf91122/dbGaP1/ARIC/ARIC.1KG.imputed
+
+setwd("/scratch/mf91122/dbGaP1/ARIC/ARIC.1KG.imputed")
+
+list.files()
+
+load("ARIC.AA.chr9.RData")
+ls() #list objects that exist in environment
+write.table(geno, "ARIC.AA.chr9.vcf")
