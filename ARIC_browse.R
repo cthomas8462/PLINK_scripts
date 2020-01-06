91194b165a6175aebd0346d4cc2afc049a5768de
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-06 13:00:16 -0500

    Update ARIC_browse.R

--- ARIC_browse.R
+++ ARIC_browse.R
@@ -6,5 +6,7 @@ setwd("/scratch/mf91122/dbGaP1/ARIC/ARIC.1KG.imputed")
 list.files()
 
 load("ARIC.AA.chr9.RData")
-ls() #list objects that exist in environment
+#ls() #list objects that exist in environment
+
+colnames(geno)[1]<-"#CHROM"
 write.table(geno, "ARIC.AA.chr9.vcf")
