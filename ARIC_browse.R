040a31cb166559514ac2eec480799f4046944ea7
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-20 15:25:15 -0500

    Update ARIC_browse.R

--- ARIC_browse.R
+++ ARIC_browse.R
@@ -10,3 +10,27 @@ load("ARIC.AA.chr9.RData")
 
 colnames(geno)[1]<-"#CHROM"
 write.table(geno, "ARIC.AA.chr9.vcf")
+
+
+#01-20-2020 write script that finds snps from table
+library(tidyverse)
+
+
+setwd("/scratch/mf91122/dbGaP1/ARIC/")
+snplist<-read.csv("testsnplist.csv")
+colnames(snplist)<-c("CHROM", "POS")
+
+
+files<- grep("ARIC.EA.chr[0-9]+.p[0-9]+.RData", as.character(list.files(path="ARIC.1KG.imputed/")), value=TRUE)
+setwd("/scratch/mf91122/dbGaP1/ARIC/ARIC.1KG.imputed/")
+
+x<-list()
+for (i in seq_along(files)){
+  load(files[i])
+  geno1<-as.data.frame(geno)
+  match<-geno1[,2] %in% snplist[,2] & geno1[,1] %in% snplist[,1]
+  x<-c(x, geno1[match,])}
+  
+  
+  
+  
