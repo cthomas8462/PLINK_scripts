293071b6a873fe681fe02125d3dc644e47a66398
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-21 12:25:51 -0500

    Update ARIC_pheno.sh

--- ARIC_pheno.sh
+++ ARIC_pheno.sh
@@ -5,5 +5,9 @@ cat phs000280.v3.pht004063.v1.p1.c1.DERIVE13.HMB-IRB.txt |head -20| cut -f20,21
 
 cat phs000280.v3.pht004238.v1.p1.c1.VITA03.HMB-IRB.txt |cut -f26,67 > ../myARIC/ARIC_fish_oil.txt
 
-cat phs000280.v3.pht004063.v1.p1.c1.DERIVE13.HMB-IRB.txt |head -20| cut -f19,20,27,110,120,122,
+
+cat phs000280.v3.pht004063.v1.p1.c1.DERIVE13.HMB-IRB.txt |head -20| cut -f19,20,21,27,33,35,36,37,106,110,120,122
+
+#need fish servings
+
 
